0bba7349d02b94358c8c156add81263bd4c8ac96
David B. Cortarello <dcortarello@gmail.com>
2014-03-04 01:21:59 -0300

    Changed homepage

--- src/ktsuss.c
+++ src/ktsuss.c
@@ -74,9 +74,9 @@ void say_help(char *str)
 /* Print's the about text to the terminal and exits with no error */
 void say_about(void)
 {
-	printf("%s - Copyright (c) 2007-2011 David B. Cortarello\n\n", PACKAGE_STRING);
+	printf("%s - Copyright (c) 2007-2014 David B. Cortarello\n\n", PACKAGE_STRING);
 	printf("Please report comments, suggestions and bugs to:\n\t%s\n\n", PACKAGE_BUGREPORT);
-	printf("Check for new versions at:\n\thttp://nomius.blogspot.com\n\n");
+	printf("Check for new versions at:\n\thttps://github.com/nomius/ktsuss\n\n");
 	exit(0);
 }
 
