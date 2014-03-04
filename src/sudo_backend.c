47d9262defccc351a0b5f408b1c3c9e7bf0c60fc
David B. Cortarello <dcortarello@gmail.com>
2014-03-04 01:08:25 -0300

    Applied patch by possum@possum.cc. Also ignored stderr and improved return status in password check

--- src/sudo_backend.c
+++ src/sudo_backend.c
@@ -43,6 +43,7 @@
 #include <pty.h>
 #include <utmp.h>
 #include <termios.h>
+#include <fcntl.h>
 
 #include "errors.h"
 
@@ -58,7 +59,7 @@ static struct termios orig_termios;
 int check_password_sudo(const char *username, const char *password)
 {
 	int status = 0, pid = 0, pip[2];
-	char *cmd[7] = { SUDOPATH, "-u", (char *)username, "-S", "-E", "true", NULL };
+	char *cmd[10] = { SUDOPATH, "-u", (char *)username, "-k", "-S", "-p", "", "-E", "true", NULL };
 	char pass[64];
 
 	if (pipe(pip)) err(1, "pipe()");
@@ -66,6 +67,9 @@ int check_password_sudo(const char *username, const char *password)
 	if ((pid = fork()) < 0) err(1, "fork()");
 	else if (pid == 0) {
 		close(pip[1]);
+		status = open("/dev/null", O_WRONLY);
+		dup2(status, STDERR_FILENO);
+		close(status);
 		dup2(pip[0], STDIN_FILENO);
 		execv(cmd[0], cmd);
 		close(pip[0]);
@@ -77,31 +81,49 @@ int check_password_sudo(const char *username, const char *password)
 	close(pip[1]);
 	
 	waitpid(pid, &status, 0);
-	if (!WIFEXITED(status))
-		return -1;
-	if (WEXITSTATUS(status) != 0)
-		return ERR_WRONG_USER_OR_PASSWD;
-	return ERR_SUCCESS;
+	if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+		return ERR_SUCCESS;
+	else
+		if (WIFSIGNALED(status)) {
+			fprintf(stderr, "Why I was signaled?: %d\n", WTERMSIG(status));
+			return -1;
+		}
+	return ERR_WRONG_USER_OR_PASSWD;
 }
 
 
 /* Run the given command as the given user */
 void run_sudo(char *username, char *password, char *command)
 {
-	char buf[BUFF_SIZE], *cmd[7] = { SUDOPATH, "-u", (char *)username, "-E", command, NULL };
+	char buf[BUFF_SIZE], *cmd[10] = { SUDOPATH, "-u", (char *)username, "-k", "-S", "-p", "", "-E", command, NULL };
 	int fdpty = 0, status = 0, tty = 1, i = 0;
+	int pip[2];
 	pid_t pid;
 	fd_set rfds;
 	struct timeval tv;
+
+	char pass[64];
+
+	if (pipe(pip)) err(1, "pipe()");
 
 	/* Creates a new terminal */
 	if ((pid = forkpty(&fdpty, NULL, NULL, NULL)) < 0) err(1, "forkpty()");
 	else if (pid == 0) {
 		setsid();
+
+		close(pip[1]);
+		dup2(pip[0], STDIN_FILENO);
 		execv(cmd[0], cmd);
+		close(pip[0]);
+
 		err(1, "execv()");
 		exit(1);
 	}
+
+    close(pip[0]);
+    sprintf(pass, "%s\n", password);
+    write(pip[1], pass, strlen(pass));
+    close(pip[1]);
 
 	/* Put the terminal in raw mode */
 	if (tcgetattr(STDIN_FILENO, &orig_termios) < 0) {
@@ -113,6 +135,7 @@ void run_sudo(char *username, char *password, char *command)
 	if (tty)
 		tty_raw(STDIN_FILENO);
 
+	/*read(fdpty, buf, BUFF_SIZE);*/
 	while (!waitpid(pid, &status, WNOHANG)) {
 
 		/* Ok, the program needs some interaction, so this will do it fine */
